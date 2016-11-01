b2052924e0ca72c9f8a0e7ea22265fa625c5bc06
nzucolillo <nzucolillo@sistemas-pc.randazzo.local>
2016-11-01 16:49:52 -0300

    primer

---
+++ src/emailSuscripcionesRandazzo/Suscriptor.java
@@ -0,0 +1,23 @@
+package emailSuscripcionesRandazzo;
+
+
+public class Suscriptor {
+	
+		private String nyap;
+		private String email;
+	public Suscriptor(String nyap,String email)
+	{
+		this.nyap= nyap;
+		this.email= email;
+	}
+	public String getNyap(){
+		return this.nyap;
+	}
+	
+	public String getEmail(){
+		return this.email;
+	}
+	
+	
+}
+
---
+++ src/emailSuscripcionesRandazzo/main.java
@@ -0,0 +1,38 @@
+package emailSuscripcionesRandazzo;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import javax.swing.SingleSelectionModel;
+
+public class main {
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		try {			
+			String[] mes={"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+			java.util.Date fecha = new java.util.Date();//creo una clase Date para que me de la fecha actual
+			int m= fecha.getMonth(); //obtengo el numero de mes actual
+			Conectar con= new Conectar();
+			con.Abrir();
+			con.consultaTablaSuscriptores();
+			ArrayList sus= con.getSuscriptores();
+		    int size=sus.size();	    	
+	
+		    for(int x=0; x<size; x++) {		    	
+		    	HtmlMail mail = new HtmlMail();
+		    	String contenidoHTML = HtmlMail.loadHTMLFile(((Suscriptor)sus.get(x)).getNyap(),mes[m]);//		    	
+		    	mail.addContent(contenidoHTML);
+		    	mail.addCID("image","E:/emailSuscripcionRandazzo/kiara_1.jpg");
+				mail.setTo(((Suscriptor)sus.get(x)).getEmail());//((Suscriptor)sus.get(x)).getEmail()
+				mail.sendMultipart();
+
+				//Thread.sleep(1000);
+		}			
+			System.out.println("[ Mail enviado ]");
+			} catch (Exception e) {
+			e.printStackTrace();
+			}
+}
+
+
+}
